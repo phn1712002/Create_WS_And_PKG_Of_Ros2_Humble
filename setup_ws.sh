7ef03a921bbdb8ba2be494a5fc765fb3077a29ab
HoangNam <phn1712002@gmail.com>
2024-05-07 23:08:28 +0700

    feat: :white_check_mark: Cập nhật code mới nhất

--- setup_ws.sh
+++ setup_ws.sh
@@ -22,19 +22,10 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 # Echo ~/.bashrc in user ubuntu
 echo "" >> /home/ubuntu/.bashrc
 echo "#### THIS IS OF [$DIR]" >> /home/ubuntu/.bashrc
-echo "cd $DIR" >> /home/ubuntu/.bashrc
 echo "source $DIR/install/local_setup.bash" >> /home/ubuntu/.bashrc
 echo "########################" >> /home/ubuntu/.bashrc
 echo "" >> /home/ubuntu/.bashrc
 echo "clear" >> /home/ubuntu/.bashrc
-
-# Echo ~/.bashrc in user root
-echo "" >> /root/.bashrc
-echo "#### THIS IS OF [$DIR]" >> /root/.bashrc
-echo "source $DIR/install/local_setup.bash" >> /root/.bashrc
-echo "########################" >> /root/.bashrc
-echo "" >> /root/.bashrc
-echo "clear" >> /root/.bashrc
 
 # Remove git
 while true; do
@@ -51,4 +42,5 @@ esac
 done
 # Echo Complete
 clear
+history -c 
 echo "Complete !"
