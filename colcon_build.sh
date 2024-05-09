83729cf7bf3fe6124202ef51bd1ebbf7ff283378
HoangNam <phn1712002@gmail.com>
2024-05-09 13:42:34 +0700

    feat: :white_check_mark: Cập nhật code mới

--- colcon_build.sh
+++ colcon_build.sh
@@ -3,7 +3,8 @@ clear
 echo "1. Colcon build"
 echo "2. Colcon build with remove build/install/log"
 echo "3. Colcon build with packages select"
-echo "4. Install symlink"
+echo "4. Colcon build with packages skip"
+echo "5. Install symlink"
 echo "===================================================="
 echo "Select?"
 read SELECT
@@ -20,6 +21,9 @@ case "$SELECT" in
    colcon build --packages-select $NAME_PKG 
    ;;
    "4") 
+   colcon build --packages-skip $NAME_PKG	
+   ;;
+   "5") 
    rm -rf build install log
    colcon build --symlink-install	
    ;;
