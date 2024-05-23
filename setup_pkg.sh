d4dd7e8709dff171d7767caa31193d9ae2a9e1fa
HoangNam <phn1712002@gmail.com>
2024-05-23 19:58:44 +0700

    feat: :white_check_mark: Cập nhật code

--- setup_pkg.sh
+++ setup_pkg.sh
@@ -1,10 +1,10 @@
 clear
 # Create package
-echo "Name of package?"
+echo "Name of package?: "
 read PKG_NAME
 cd src
 while true; do
-read -p "C++ or Python, create package with C++ (y/n) ?" yn
+read -p "C++ or Python, create package with C++ (y/n) ?: " yn
 case $yn in 
 	[yY] ) echo "Create package with C++";
 		ros2 pkg create --build-type ament_cmake $PKG_NAME
